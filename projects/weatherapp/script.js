d3a2b869b43a9068b951cb885bc2aa2affa03eb5
haegis999 <116662815+haegis999@users.noreply.github.com>
2023-03-22 18:07:14 +0100

    Update script.js

--- projects/weatherapp/script.js
+++ projects/weatherapp/script.js
@@ -1,54 +1,45 @@
 let weather = {
     apiKey: "e3eb45e31337d2dd04cd94be5a2bb746",
-    fetchWeather: function (city) {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          city +
-          "&units=metric&appid=" +
-          this.apiKey
-      )
-        .then((response) => {
-          if (!response.ok) {
-            alert("No weather found.");
-            throw new Error("No weather found.");
-          }
-          return response.json();
-        })
-        .then((data) => this.displayWeather(data));
+    fetchWeather: function(city) {
+      $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey,
+        method: "GET",
+        success: function(data) {
+          weather.displayWeather(data);
+        },
+        error: function() {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+      });
     },
-    displayWeather: function (data) {
+    displayWeather: function(data) {
       const { name } = data;
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
-      document.querySelector(".weather").classList.remove("loading");
-      document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      $(".city").text("Weather in " + name);
+      $(".icon").attr("src", "https://openweathermap.org/img/wn/" + icon + ".png");
+      $(".description").text(description);
+      $(".temp").text(temp + "°C");
+      $(".humidity").text("Humidity: " + humidity + "%");
+      $(".wind").text("Wind speed: " + speed + " km/h");
+      $(".weather").removeClass("loading");
+      $("body").css("background-image", "url('https://source.unsplash.com/1600x900/?" + name + "')");
     },
-    search: function () {
-      this.fetchWeather(document.querySelector(".search-bar").value);
+    search: function() {
+      this.fetchWeather($(".search-bar").val());
     },
   };
   
-  document.querySelector(".search button").addEventListener("click", function () {
+  $(".search button").on("click", function() {
     weather.search();
   });
   
-  document
-    .querySelector(".search-bar")
-    .addEventListener("keyup", function (event) {
-      if (event.key == "Enter") {
-        weather.search();
-      }
-    });
+  $(".search-bar").on("keyup", function(event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
   
   weather.fetchWeather("Lagos");
