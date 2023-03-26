68d3380b9d655b26e6b01357bfbd4b88d18f9284
haegis999 <116662815+haegis999@users.noreply.github.com>
2023-03-26 13:03:55 +0100

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -1,18 +1,25 @@
 <?php
-if (isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $to = 'takinyemi79@gmail.com'; // replace with your email address
-  $subject = 'New message from ' . $name;
-  $body = "Name: $name\n\nEmail: $email\n\nMessage: $message";
-  $headers = "From: $email";
-
-  if (mail($to, $subject, $body, $headers)) {
-    echo '<p>Your message has been sent.</p>';
-  } else {
-    echo '<p>Sorry, there was an error sending your message. Please try again later.</p>';
-  }
-}
-?>
+if(isset($_POST['email'])) {
  
+    // EDIT THE FOLLOWING TWO LINES:
+    $email_to = "your-email@example.com";
+    $email_subject = "Contact form submitted";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
